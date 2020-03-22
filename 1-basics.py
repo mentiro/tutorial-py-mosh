80e51c64f9b5f4153ea8b6449fbd749ca62b2ae5
Andrei Calopareanu <mentiro@gmail.com>
2020-03-22 15:15:20 +0100

    First commit

---
+++ 1-basics.py
@@ -0,0 +1,11 @@
+#Variables
+name = "John Smith"
+age = 20
+is_new = True
+
+#Input & Print
+name = input("What is your name? ")
+color = input("What is your favourite color " + name + "? ")
+print(name + " likes " + color)
+
+#
