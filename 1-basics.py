2c85f2ee16741cf26a001136fc32580500bded32
Andrei Calopareanu <mentiro@gmail.com>
2020-03-22 17:46:43 +0100

    course progress

--- 1-basics.py
+++ 1-basics.py
@@ -3,9 +3,20 @@ name = "John Smith"
 age = 20
 is_new = True
 
+
 #Input & Print
+"""
 name = input("What is your name? ")
 color = input("What is your favourite color " + name + "? ")
 print(name + " likes " + color)
+"""
+
+
+#Type Convert
+weight_lbs = input("What's your weight in Pounds? ")
+lbs2kg_ratio = 0.45359237 #1 pound = 0.45359237 kg
+weight_kg = int(weight_lbs) * lbs2kg_ratio
+print("You weight " + str('%.2f' % weight_kg) + " kg!") # '%.2f' %  - limits to 2 decimal points
+
 
 #
