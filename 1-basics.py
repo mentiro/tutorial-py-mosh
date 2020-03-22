2e35a6cc71222d31bf996d65e2dfbfdf4be59afa
Andrei Calopareanu <mentiro@gmail.com>
2020-03-23 00:15:36 +0100

    formatting changes

--- 1-basics.py
+++ 1-basics.py
@@ -1,24 +1,20 @@
 #Variables
-name = "John Smith"
-age = 20
-is_new = True
+# name = "John Smith"
+# age = 20
+# is_new = True
 
 
 #Input & Print
-"""
-name = input("What is your name? ")
-color = input("What is your favourite color " + name + "? ")
-print(name + " likes " + color)
-"""
+# name = input("What is your name? ")
+# color = input("What is your favourite color " + name + "? ")
+# print(name + " likes " + color)
 
 
 #Type Conversion
-"""
-weight_lbs = input("What's your weight in Pounds? ")
-lbs2kg_ratio = 0.45359237 #1 pound = 0.45359237 kg
-weight_kg = int(weight_lbs) * lbs2kg_ratio
-print("You weight " + str('%.2f' % weight_kg) + " kg!") # '%.2f' %  - limits to 2 decimal pointsg
-"""
+# weight_lbs = input("What's your weight in Pounds? ")
+# lbs2kg_ratio = 0.45359237 #1 pound = 0.45359237 kg
+# weight_kg = int(weight_lbs) * lbs2kg_ratio
+# print("You weight " + str('%.2f' % weight_kg) + " kg!") # '%.2f' %  - limits to 2 decimal pointsg
 
 
 #Strings
