352d27b31d5ba51c23316478c6eb3cecaece5599
Andrei Calopareanu <mentiro@gmail.com>
2020-03-25 22:01:05 +0100

    course progression

--- 1-basics.py
+++ 1-basics.py
@@ -1,20 +1,78 @@
-#Variables
+###Variables
 # name = "John Smith"
 # age = 20
 # is_new = True
 
 
-#Input & Print
+###Input & Print
 # name = input("What is your name? ")
 # color = input("What is your favourite color " + name + "? ")
 # print(name + " likes " + color)
 
 
-#Type Conversion
+###Type Conversion
 # weight_lbs = input("What's your weight in Pounds? ")
 # lbs2kg_ratio = 0.45359237 #1 pound = 0.45359237 kg
 # weight_kg = int(weight_lbs) * lbs2kg_ratio
-# print("You weight " + str('%.2f' % weight_kg) + " kg!") # '%.2f' %  - limits to 2 decimal pointsg
+# print("You weight " + str('%.2f' % weight_kg) + " kg!")       #'%.2f' %  - limits to 2 decimal pointsg
 
 
-#Strings
+###Strings & formatted Strings
+# car1="BMW"
+# car2='Mercedes'
+# car3="M2"
+# other_cars='''
+# volkswagen
+# also audi
+# and maybe subaru
+# '''
+# print(car1)
+# print(car2[0])
+# print(car3[-1])
+# print(car2[0:4])    #returns the characters from 0 to 3
+# print(car2[0:])     #returns the charactes to the end of the string
+#
+# first = "John"
+# last = "Smith"
+# msg1 = "My name is " + first + " " + last
+# msg2 = f'My name is {first} {last}'     #formatted string - adding f and curly brackets
+# print(msg1)
+# print(msg2)
+
+
+###String Methods
+# planet = "Mars"
+# location = "Solar system"
+#
+# print(len(planet))
+#
+# print(planet.upper())
+# print(planet.lower())
+# print(planet.find('M'))             #returns the index of the first occurance of M
+# print(planet.replace('M','C'))      #replaces M with C
+# print(planet.replace('m','C'))      #does nothing - shows this method is case sensitive
+# print("system" in location)         #returns True - searches for the work "system" in the location Variable
+
+
+###Arithmetic Operators & Function
+# "The usual Operators"
+# a = 5/3
+# b = 5//3    #result is an int
+# print(a)
+# print(b)
+# x = 5
+# x += 2      #shorter version for x = x + 2
+# print(x)
+# #Functions
+# y=3.14
+# print(round(y))
+# z=-33
+# print(abs(z))       #prints the absolute (positive) value of the variable/number
+#
+# import math              #this imports the "math" module - now we can access various methods (functions) used for various calculations
+# print(math.ceil(y))      #prints ceiling of the Variable
+# print(math.floor(y))     #prints floor of the Variable
+
+
+
+### Left at - 58:22 (If Statements) ###
