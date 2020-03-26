d609f1f6b3ebd7b97fe5ccf34e2aa8910d07d15e
Andrei Calopareanu <mentiro@gmail.com>
2020-03-26 01:23:19 +0100

    - minor changes - if statements

--- 1-basics.py
+++ 1-basics.py
@@ -14,7 +14,7 @@
 # weight_lbs = input("What's your weight in Pounds? ")
 # lbs2kg_ratio = 0.45359237 #1 pound = 0.45359237 kg
 # weight_kg = int(weight_lbs) * lbs2kg_ratio
-# print("You weight " + str('%.2f' % weight_kg) + " kg!")       #'%.2f' %  - limits to 2 decimal pointsg
+# print("You weight " + str('%.2f' % weight_kg) + " kg!")       #'%.2f' %  - limits to 2 decimal points
 
 
 ###Strings & formatted Strings
---
+++ 2-cond-loops.py
@@ -0,0 +1,28 @@
+###If Statements
+# one = False
+# two = False
+#
+# if one:
+#     print("This is One because it's " + str(one))
+# elif two:
+#     print("This is Two because it's " + str(two))
+# else:
+#     print("Neither One nor Two because they are both " + str(one))
+
+#Exercise
+# Price of a house is $1M
+# If a buyer has good credit,
+#   they neet to put down 10%
+# Otherwise
+#   they need to put down 20%
+#Print the down payment
+
+price = 1000000
+good_credit = False
+
+if good_credit:
+    downpayment = price*0.1
+else:
+    downpayment = price*0.2
+print("You will need to put down $" + str('%.0f' % downpayment))    #"'%.0f' % downpayment" - 0 decimal points
+print(f"You will need to put down ${'%.0f' % downpayment}")         #same, but with formatted string
